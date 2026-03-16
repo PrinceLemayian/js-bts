cce43561a1c804f0607310c0068cf4019465cd79
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-03-16 23:44:28 +0300

    program to practice using the this keyword

---
+++ this.js
@@ -0,0 +1,30 @@
+'use strict';
+
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2026 - birthYear);
+  console.log(this);
+};
+calcAge(2007);
+
+const calcAgeArrow = birthYear => {
+  console.log(2026 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(2006);
+
+const lemayian = {
+  year: 2007,
+  calcAge: function () {
+    console.log(this);
+    console.log(2026 - this.year);
+  },
+};
+lemayian.calcAge();
+
+const thorfinn = {
+  year: 2006,
+};
+
+thorfinn.calcAge = lemayian.calcAge;
